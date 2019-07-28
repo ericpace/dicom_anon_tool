11c4770b859b5f6251b252f08c8e659b7e2fc455
Eric_MacBookPro <ericpace@gmail.com>
2019-07-29 08:27:00 +0900

    Handle input arguments

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import os
+import argparse
 from collections import namedtuple
 import anonymiser as anon
 import pydicom
 from pathlib import Path
-from pydicom.data import get_testdata_files
-from pydicom.tag import Tag
 
+parser = argparse.ArgumentParser(description='Anonymise DICOM images')
+parser.add_argument('source', metavar='S', type=str,
+                    help='location of dicom file to anonymise')
+parser.add_argument('-d', '--destination',
+                    help='location where to save anonymised DICOM file')
+parser.add_argument('-t', '--tagfile', default="user_tags.txt",
+                    help='path to custom tags file')
 
+args = parser.parse_args()
+
+print(args.source, args.destination, args.tagfile)
 Point = namedtuple('Point', ['x', 'y'])
 Filter = namedtuple('Filter', ['id', 'description', 'value'])
 
-filepath = "/Users/ericpace/Desktop/Work/PHY1/PHY1.Seq12.Ser11.Img0.dcm"
+# Other filepaths for testing
+# filepath = "/Users/ericpace/Desktop/Work/PHY1/PHY1.Seq12.Ser11.Img0.dcm"
 # filepath = get_testdata_files("rtplan.dcm")[0]
 # filepath = "test_anon.dcm"
-ds = pydicom.dcmread(filepath)
 
-path = Path(filepath)
+
+ds = pydicom.dcmread(args.source)
+
+path = Path(args.source)
 
 # TODO Add argparse to handle:
 # 1. Input filename or file directory
@@ -26,8 +38,7 @@ path = Path(filepath)
 # TODO Use Path properly
 
 
-
-with open("user_tags.txt") as f:
+with open(args.tagfile) as f:
     # Read each line
     data_elements = f.read().splitlines()
 
@@ -36,8 +47,11 @@ my_tags = anon.generate_tags(data_elements)
 
 ds_anon = anon.anonymise(ds, my_tags)
 
-ds_anon.save_as(os.path.join(path.parent, f"{path.stem}_anon.dcm"))
-
+# TODO handle better
+if not args.destination:
+    ds_anon.save_as(os.path.join(path.parent, f"{path.stem}_anon.dcm"))
+else:
+    ds_anon.save_as(args.destination)
 
 
 print('finished')
