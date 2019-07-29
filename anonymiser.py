bcbb865b06dfd0760a69154ad9ff58b2af0c68fa
Eric_MacBookPro <ericpace@gmail.com>
2019-07-29 10:03:11 +0900

    Program now works with files or flat folders

--- anonymiser.py
+++ anonymiser.py
@@ -1,5 +1,8 @@
+import os
+import pydicom
 from pydicom.tag import Tag
 from collections import namedtuple
+from pathlib import Path
 Filter = namedtuple('Filter', ['id', 'description', 'value'])
 
 
@@ -10,6 +13,14 @@ def generate_tags(user_list):
         tag = Tag([int(x, 16) for x in line.id.split(",")])  # Convert hex to int and get Tag
         tags[tag] = line.value
     return tags
+
+
+def load_dicom_file(filepath):
+    try:
+        ds = pydicom.dcmread(os.path.join(filepath))
+    except pydicom.errors.InvalidDicomError:
+        print("invalid DICOM")
+    return ds
 
 
 def anonymise(ds, tags):
@@ -27,3 +38,13 @@ def anonymise(ds, tags):
 
     return ds
 
+
+def start(filepath, tags):
+    f = Path(filepath)
+    ds = load_dicom_file(filepath)
+    print(f"Opened file: {f.stem}")
+
+    ds_anon = anonymise(ds, tags)
+
+    # TODO handle better
+    ds_anon.save_as(os.path.join(f.parent, f"{f.stem}_anon.dcm"))
--- main.py
+++ main.py
@@ -5,18 +5,10 @@ import anonymiser as anon
 import pydicom
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Anonymise DICOM images')
-parser.add_argument('source', metavar='S', type=str,
-                    help='location of dicom file to anonymise')
-parser.add_argument('-d', '--destination',
-                    help='location where to save anonymised DICOM file')
-parser.add_argument('-t', '--tagfile', default="user_tags.txt",
-                    help='path to custom tags file')
 
-args = parser.parse_args()
+# TODO Add logging function
+# TODO Use Path properly
 
-print(args.source, args.destination, args.tagfile)
-Filter = namedtuple('Filter', ['id', 'description', 'value'])
 
 # Other filepaths for testing
 # filepath = "/Users/ericpace/Desktop/Work/PHY1/PHY1.Seq12.Ser11.Img0.dcm"
@@ -24,28 +16,32 @@ Filter = namedtuple('Filter', ['id', 'description', 'value'])
 # filepath = "test_anon.dcm"
 
 
-ds = pydicom.dcmread(args.source)
+parser = argparse.ArgumentParser(description='Anonymise DICOM images')
+parser.add_argument('source', metavar='S', type=str,
+                    help='location of dicom file to anonymise')
+# parser.add_argument('-d', '--destination',
+#                     help='location where to save anonymised DICOM file')
+parser.add_argument('-t', '--tagfile', default="user_tags.txt",
+                    help='path to custom tags file')
 
-path = Path(args.source)
+args = parser.parse_args()
+sourcepath = Path(args.source)
 
-# TODO Add logging function
-# TODO Use Path properly
-
+Filter = namedtuple('Filter', ['id', 'description', 'value'])
 
 with open(args.tagfile) as f:
     # Read each line
     data_elements = f.read().splitlines()
 
-
 my_tags = anon.generate_tags(data_elements)
 
-ds_anon = anon.anonymise(ds, my_tags)
-
-# TODO handle better
-if not args.destination:
-    ds_anon.save_as(os.path.join(path.parent, f"{path.stem}_anon.dcm"))
+if sourcepath.is_dir():
+    for root, dirs, files in os.walk(sourcepath):
+        print(f"Working in: {root}")
+        # TODO This currently does not traverse subdirs
+        for file_name in files:
+            anon.start(os.path.join(root, file_name), my_tags)
 else:
-    ds_anon.save_as(args.destination)
-
+    anon.start(args.source, my_tags)
 
 print('finished')
